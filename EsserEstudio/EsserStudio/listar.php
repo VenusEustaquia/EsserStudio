acc4fbe78ecd58fa2c416e91cdfe09d1cc5caa8e
Laura Abad <ifskiedthenrulz@gmail.com>
2012-08-07 13:24:06 +0200

    comm

---
+++ EsserEstudio/EsserStudio/listar.php
@@ -0,0 +1,12 @@
+<?php
+
+include('funciones.php');
+$tipo= recoge('tipo');
+cabecera('Listar canciones');
+
+       print "<p>$tipo:</p>\n<div id='lista-$tipo'></div>\n";
+
+
+$db = NULL;
+pie();
+?>
